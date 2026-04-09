0b9909dcd07074895a40c1370ca221d266907356
seamheadzapp-coder <seamheadz.app@gmail.com>
2026-04-09 17:26:39 -0400

    Update yahoo.js

--- pages/api/auth/yahoo.js
+++ pages/api/auth/yahoo.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const clientId = 'dj0yJmk9b3luZWl3Qm1GSjBPJmQ9WVdrOU9WVkJbFl3Um1RbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTI2'
+  const clientId = 'dj0yJmk9b3luZWl3Qm1GSjBPJmQ9WVdrOU9WVkJXbFl3Um1RbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTI2'
   const redirectUri = 'https://seamheadz.vercel.app/api/auth/callback'
 
   const params = new URLSearchParams({
